0a69e4d5ad27b542b2019f258aea96b5373c1c91
leommelo <lmmelo096@gmail.com>
2024-10-27 23:37:41 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,32 @@
+public class C {
+    private int C1;
+    private float C2;
+
+    // Getters e Setters
+    public int getC1() {
+        return C1;
+    }
+
+    public void setC1(int C1) {
+        this.C1 = C1;
+    }
+
+    public float getC2() {
+        return C2;
+    }
+
+    public void setB2(float B2) {
+        this.C2 = C2;
+    }
+
+    // Métodos
+    public void MC1() {
+        System.out.println("Método MC1");
+    }
+
+    public void MC2() {
+        System.out.println("Método MC2");
+    }
+}
+
+
