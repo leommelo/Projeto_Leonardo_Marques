3d09b0964bbf96e768bfe6fec9ab1e8d0294f5f5
leommelo <lmmelo096@gmail.com>
2024-10-27 22:44:29 -0300

    Classes A e B criadas

---
+++ B.java
@@ -0,0 +1,30 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    // Getters e Setters
+    public int getB1() {
+        return B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+    // Métodos
+    public void MB1() {
+        System.out.println("Método MB1");
+    }
+
+    public void MB2() {
+        System.out.println("Método MB2");
+    }
+}
