144ac9f1d3f9463b6b868ad7b63ffc23db009db9
leommelo <lmmelo096@gmail.com>
2024-10-28 00:26:49 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,30 @@
+public class D {
+    private String D1;
+    private double D2;
+
+    // Getters e Setters
+    public String getD1() {
+        return D1;
+    }
+
+    public void setD1(String D1) {
+        this.D1 = D1;
+    }
+
+    public double getD2() {
+        return D2;
+    }
+
+    public void setD2(double D2) {
+        this.D2 = D2;
+    }
+
+    // Métodos
+    public void MD1() {
+        System.out.println("Método MD1");
+    }
+
+    public void MD2() {
+        System.out.println("Método MD2");
+    }
+}
